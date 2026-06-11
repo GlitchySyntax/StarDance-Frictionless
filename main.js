90efab0b11ebba87f2a44e40ecdd6a7fb148e6a5
Muhammad Ahmad Iftikhar <mirahmadiftikhar@icloud.com>
2026-06-12 02:09:06 +0500

    Added additional library for interface along with a port number. Also added a sanity check for the main directories that will be in use

--- main.js
+++ main.js
@@ -2,10 +2,15 @@ import { execSync } from 'child_process';
 import fs from 'fs';
 import path from 'path';
 import os from 'os';
+import http from 'http';
 
 
 //Adding the main directories that will be used
-
+const PORT = 3000;
 const HOME = os.homedir();
 const WATCH_LATER_DIR = path.join(HOME, 'WatchLater');
 const READ_LATER_DIR = path.join(HOME, 'ReadLater');
+
+//checking if the directories exist or not and if they don't exist then creating them
+if (!fs.existsSync(WATCH_LATER_DIR)) fs.mkdirSync(WATCH_LATER_DIR, { recursive: true });
+if (!fs.existsSync(READ_LATER_DIR)) fs.mkdirSync(READ_LATER_DIR, { recursive: true });
