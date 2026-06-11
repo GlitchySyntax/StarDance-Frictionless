3733dded111564aa456a886546f9ea7f90ab8527
Muhammad Ahmad Iftikhar <mirahmadiftikhar@icloud.com>
2026-06-12 02:19:21 +0500

    Directory check through console.log

--- main.js
+++ main.js
@@ -14,3 +14,7 @@ const READ_LATER_DIR = path.join(HOME, 'ReadLater');
 //checking if the directories exist or not and if they don't exist then creating them
 if (!fs.existsSync(WATCH_LATER_DIR)) fs.mkdirSync(WATCH_LATER_DIR, { recursive: true });
 if (!fs.existsSync(READ_LATER_DIR)) fs.mkdirSync(READ_LATER_DIR, { recursive: true });
+
+console.log("Directories verified.");
+console.log(`Videos path: ${WATCH_LATER_DIR}`);
+console.log(`Articles path: ${READ_LATER_DIR}`);
